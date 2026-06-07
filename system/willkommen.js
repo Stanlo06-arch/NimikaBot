3efdf42c09189a44bb4f6f20f5ab749582a18487
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 13:11:58 +0200

    Update willkommen.js

--- system/willkommen.js
+++ system/willkommen.js
@@ -1,1 +1,14 @@
+const { welcomeChannel } = require("../config/ids");
 
+module.exports = (client) => {
+client.on("guildMemberAdd", async (member) => {
+const channel = member.guild.channels.cache.get(welcomeChannel);
+
+```
+if (!channel) return;
+
+await channel.send(`🌸 Willkommen ${member}!`);
+```
+
+});
+};
