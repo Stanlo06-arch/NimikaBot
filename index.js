828f942c6bae1be5cd96961ece79aba992790fea
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 13:12:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMembers
+]
+});
+
+client.once("ready", () => {
+console.log(`🌸 Bot online: ${client.user.tag}`);
+});
+
+require("./system/willkommen")(client);
+
+client.login(process.env.TOKEN);
