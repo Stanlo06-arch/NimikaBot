9a0cd40bd62390ffa588567599c378147156e4b2
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 13:01:46 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,26 @@
+module.exports = (client) => {
+client.on('guildMemberAdd', async (member) => {
+const channel = member.guild.channels.cache.get(
+process.env.WELCOME_CHANNEL_ID
+);
 
+```
+if (!channel) return;
+
+channel.send({
+  content: `# 🌸 Willkommen auf meinem Server! ❤️
+```
+
+Hey ${member} 🫶
+
+Schön, dass du den Weg auf unseren Server gefunden hast!
+
+Hier erwartet dich eine freundliche Community,
+Gaming, Animal Crossing, gemütliche Streams
+und viele schöne Momente mit anderen Spielern. 🌷
+
+Wir wünschen dir ganz viel Spaß und freuen uns,
+dich bei uns begrüßen zu dürfen! ✨`
+});
+});
+};
