569e1676b3bee61062af2fec875bf7779b1a3692
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 19:43:06 +0200

    Update serverLogs.js

--- system/serverLogs.js
+++ system/serverLogs.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder } = require("discord.js");
 
-const LOG_CHANNEL_ID = "1490111197855617064";
+const LOG_CHANNEL_ID = "1513199769961627769";
 
 module.exports = (client) => {
   client.once("ready", async () => {
