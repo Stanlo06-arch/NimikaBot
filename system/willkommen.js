9671cd42518a327417cf1d1b10b4837c4939d7c9
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 13:49:51 +0200

    Update willkommen.js

--- system/willkommen.js
+++ system/willkommen.js
@@ -3,27 +3,26 @@ const Canvas = require("canvas");
 const { welcomeChannel } = require("../config/ids");
 
 module.exports = (client) => {
-client.on("guildMemberAdd", async (member) => {
-const channel = member.guild.channels.cache.get(welcomeChannel);
-  
-if (!channel) return;
+  client.on("guildMemberAdd", async (member) => {
+    const channel = member.guild.channels.cache.get(welcomeChannel);
 
-const canvas = Canvas.createCanvas(1200, 630);
-const ctx = canvas.getContext("2d");
+    if (!channel) return;
 
-ctx.fillStyle = "#FFF7FB";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const canvas = Canvas.createCanvas(1200, 630);
+    const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#000000";
-ctx.font = "40px Sans";
-ctx.fillText("🌸 Willkommen auf meinem Server! ❤️", 50, 100);
+    ctx.fillStyle = "#FFF7FB";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const attachment = new AttachmentBuilder(
-  canvas.toBuffer("image/png"),
-  { name: "welcome.png" }
-);
+    ctx.fillStyle = "#000000";
+    ctx.font = "40px Sans";
+    ctx.fillText("Willkommen auf meinem Server!", 50, 100);
 
-await channel.send({ files: [attachment] });
+    const attachment = new AttachmentBuilder(
+      canvas.toBuffer("image/png"),
+      { name: "welcome.png" }
+    );
 
-});
+    await channel.send({ files: [attachment] });
+  });
 };
