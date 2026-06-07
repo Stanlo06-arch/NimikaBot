3ffc54fd01d05e0f0df81ba9a7deed22407f8eed
Stanlo06-arch <Stanlo06@web.de>
2026-06-07 14:04:01 +0200

    Update willkommen.js

--- system/willkommen.js
+++ system/willkommen.js
@@ -29,13 +29,13 @@ dich bei uns begrüßen zu dürfen! ✨`
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
       .setImage("https://cdn.discordapp.com/attachments/1510626895627288676/1513141910791913533/NimikaBanner.png")
       .setFooter({
-        text: "🌸 Hostet by 𝔖𝔱𝔞𝔫𝔩𝔢𝔶_𝔯𝔪𝔭.06 ♕"
-      })
-      .setTimestamp();
+  text: "Hostet by 𝔖𝔱𝔞𝔫𝔩𝔢𝔶_𝔯𝔪𝔭.06 ♕",
+  iconURL: "https://cdn.discordapp.com/attachments/1510626895627288676/1513151420835168347/123.png"
+})
+.setTimestamp();
 
     await channel.send({
-      content: `${member}`,
-      embeds: [embed]
-    });
+  embeds: [embed]
+});
   });
 };
